d4ec8343da369f74727bde7a7f75b557ee5ab236
Dami Dina <me@damidina.com>
2019-01-15 13:33:15 -0500

    added this.state

--- src/components/landing/HowItWorks.js
+++ src/components/landing/HowItWorks.js
@@ -40,7 +40,7 @@ class HowItWorks extends React.Component {
               : { marginBottom: '6rem', maxWidth: '100%' }
             }>How It Works</h1>
 
-            {someCondition &&
+            {this.state.someCondition &&
               <div className="videomain">
                 <video class="video-container video-container-overlay" autoplay="true" loop="true" muted="true" data-reactid=".0.1.0.0">
                   <source type="video/mp4" data-reactid=".0.1.0.0.0" src="images/makeuivideo.mp4" />
